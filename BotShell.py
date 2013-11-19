136dc123f21f8aaaad63153117ede5eda6fe8dec
ichaleynbin <ichaleynbin@gmail.com>
2013-11-19 16:39:28 -0500

    new file:   BotShell.py 	new file:   Code/BotTalking.py 	new file:   Code/Commands.txt 	new file:   Code/HighScores.py 	new file:   Code/MasterBotter.py 	new file:   Code/Runcode.py 	new file:   Code/__init__.py 	new file:   Code/cmds.py 	new file:   Code/importData.py 	new file:   Code/timesave 	new file:   Code/zipIt.py 	new file:   NewData/BotsPickle 	new file:   NewData/ChannelsPickle 	new file:   NewData/Data.txt 	new file:   NewData/Data.txt~ 	new file:   NewData/DevsPickle 	new file:   NewData/FriendsPickle 	new file:   NewData/LinksPickle 	new file:   NewData/Log.txt 	new file:   NewData/LoggedInfo 	new file:   NewData/LoginInfoPickle 	new file:   NewData/OpsPickle 	new file:   NewData/OwnersPickle 	new file:   NewData/PermLoggedInfoPickle 	new file:   NewData/UsersPickle 	new file:   NewData/suggestions.txt 	modified:   README.md 	new file:   addiLocs 	new file:   timesave

---
+++ BotShell.py
@@ -0,0 +1,39 @@
+from Code import importData
+importData.initData()
+import socket
+import time
+from Code import Runcode, BotTalking, MasterBotter,cmds
+import os
+
+#This is the main script which runs everything. 
+#Its only job is to interpret the return value from Runcode.running()
+#and execute what needs to happen based on the two variables, 
+#and then execute .running() again (unless connected == 0, which is
+#what is returned upon using the !Quit command.)
+
+run = 2
+Connected = 1
+while Connected:
+    Socker = socket.socket()
+    os.system('dir')
+    Storg = Runcode.Datas(open('UserData/Data.txt','r'))
+    Runcode.init(Socker,Storg)
+    Connected,run = 1,1;print "Connected!"
+    while run >=0:
+        Storg = Runcode.Datas(open('UserData/Data.txt','r'))
+        run = 1; print "Booted"
+        while run > 0:
+            run = 2; print "Loaded"
+            while run > 1:
+                Connected,run = Runcode.running(Socker,Storg)
+            MasterBotter.Store()
+            if Connected > 0:
+                cmds.Memo = cmds.Mem()
+                reload(cmds)
+                cmds.Memo = cmds.Mem()
+        if Connected > 0:
+            reload(Runcode)
+    if Connected > 0:
+        reload(MasterBotter)
+        reload(BotTalking)
+    Socker.close()
---
+++ Code/importData.py
@@ -0,0 +1,11 @@
+import os
+
+#Only used the first time the script is run. If /userData doesn't exist,
+#create it and populate it with dummy data from /newData
+
+def initData():
+    if not os.path.isdir('./UserData/'):
+        os.mkdir('./UserData/')
+    if not os.path.isfile('./UserData/Data.txt'):
+        for item in ['ChannelsPickle','UsersPickle','BotsPickle','LoginInfoPickle','LinksPickle','Data.txt']:
+            os.system('cp ./NewData/'+item+' ./UserData/'+item+'\n')
